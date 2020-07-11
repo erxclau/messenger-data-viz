480c4bf1ca71b18092fa629aa42f8f1d329d0358
ericlau00 <sharkguy418@gmail.com>
2020-07-11 14:06:31 -0400

    Add template and util files

---
+++ app/static/js/svg/calendar.js
@@ -0,0 +1,109 @@
+import { getISOString, legend } from '../utility.js';
+
+let createCalendar = (data, start, id, domain, ncolors, legendDesc, tooltip) => {
+
+    // https://github.com/d3/d3-scale-chromatic color scale reference
+    let colors = d3.schemeBlues[ncolors];
+    colors.shift();
+
+    let colorMapper = d3.scaleQuantize()
+        .domain(domain)
+        .range(colors)
+        .nice();
+
+    let width = 954;
+    let cellSize = 16;
+    let height = cellSize * 11;
+
+    let calendar = initializeCalendarSVG(width, height, cellSize, id);
+
+    let countDay = d => d.getUTCDay();
+    let timeWeek = d3.utcSunday;
+
+    markSideLabels(calendar, cellSize, countDay);
+
+    let startOffset = timeWeek.count(d3.utcYear(start), start);
+
+    calendar.append('g')
+        .selectAll('rect')
+        .data(data)
+        .join('rect')
+        .attr('width', cellSize - 3)
+        .attr('height', cellSize - 3)
+        .attr('id', d => getISOString(d.date))
+        .attr('x', d =>
+            (timeWeek.count(d3.utcYear(start), d.date) - startOffset) * cellSize + 0.5)
+        .attr('y', d => countDay(d.date) * cellSize + 0.5)
+        .attr('fill', d => d.value == 0 ? '#F8F8FD' : colorMapper(d.value))
+        .append('title')
+        .text(tooltip);
+
+    markMonths(calendar, start);
+    legend(id, colorMapper, legendDesc, 4.5, 320, 50, 18, 0, 22, 0, 5, 'd')
+}
+
+let initializeCalendarSVG = (width, height, cellSize, id) => {
+    const svg = d3.select(`#${id}`)
+        .append('svg')
+        .attr('id', `${id}-calendar-svg`)
+        .attr('viewBox', [0, 0, width, height])
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '13px')
+
+    let legend = svg.append('g')
+        .attr('id', `${id}-legend`)
+        .attr("width", 320)
+        .attr("height", 50)
+        .attr('transform', 'translate(600,132)')
+        .attr("viewBox", [0, 0, 320, 50]);
+
+    legend.append('g').attr('id', `${id}-legend-colors`);
+    legend.append('g').attr('id', `${id}-legend-ticks-container`);
+
+    return svg.append('g')
+        .attr('transform', `translate(40.5, ${cellSize * 1.5})`);
+}
+
+let markSideLabels = (calendar, cellSize, countDay) => {
+
+    calendar.append('text')
+        .attr('x', -5)
+        .attr('y', -5)
+        .attr('font-weight', 'bold')
+        .attr('text-anchor', 'end')
+        .text('2019')
+
+    let formatDay = d => 'SMTWTFS'[d.getUTCDay()];
+
+    // this portion labels the weekdays at the left hand side of the calendar
+    // use the year 1995 because the weekdays line up correctly
+    // https://www.timeanddate.com/calendar/?year=1995&country=1
+    calendar.append('g')
+        .attr('text-anchor', 'end')
+        .selectAll('text')
+        .data(d3.range(7).map(i => new Date(1995, 0, i)))
+        .join('text')
+        .attr('x', -5)
+        .attr('y', d => (countDay(d) + 0.5) * cellSize)
+        .attr('dy', '0.31em')
+        .text(formatDay);
+}
+
+let markMonths = (calendar, start) => {
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    let tempDate = new Date(start.toISOString());
+    let endDate = new Date(tempDate.toISOString());
+    endDate.setFullYear(endDate.getFullYear() + 1);
+    for (let i = tempDate; i <= endDate; i.setMonth(i.getMonth() + 1), i.setDate(1)) {
+        let rect = document.getElementById(getISOString(i));
+
+        calendar.append('text')
+            .datum(months[i.getMonth()])
+            .attr('x', rect.getAttribute('x'))
+            .attr('y', -5)
+            .text(d => d)
+    }
+}
+
+export { createCalendar };
---
+++ app/static/js/utility.js
@@ -0,0 +1,102 @@
+let getISOString = (date) => {
+    let iso = date.toISOString();
+    return iso.substring(0, iso.indexOf('T'));
+}
+
+let isoToDate = string => new Date(`${string}T12:00:00`);
+
+function legend(
+    id,
+    color,
+    title,
+    tickSize = 6,
+    width = 320,
+    height = 44 + tickSize,
+    marginTop = 18,
+    marginRight = 0,
+    marginBottom = 16 + tickSize,
+    marginLeft = 0,
+    ticks = width / 64,
+    tickFormat,
+    tickValues) {
+
+    let x = d3.scaleLinear()
+        .domain([-1, color.range().length])
+        .rangeRound([marginLeft, width - marginRight]);
+
+    d3.select(`#${id}-legend-colors`)
+        .selectAll('rect')
+        .data(color.range())
+        .join(
+            enter => enter.append('rect')
+                .attr('x', (d, i) => x(i - 1))
+                .attr('y', marginTop)
+                .attr('width', (d, i) => x(i) - x(i - 1))
+                .attr('height', height - marginTop - marginBottom)
+                .attr('fill', d => d)
+        );
+
+    let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+    const thresholds = color.thresholds();
+    const thresholdFormat = d => Math.round(d);
+    tickValues = d3.range(thresholds.length);
+    tickFormat = i => thresholdFormat(thresholds[i], i);
+
+    d3.select(`#${id}-legend-ticks`).remove();
+
+    d3.select(`#${id}-legend-ticks-container`)
+        .append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .attr('id', `${id}-legend-ticks`)
+        .style('font-size', 11)
+        .call(d3.axisBottom(x)
+            .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+            .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+            .tickSize(tickSize)
+            .tickValues(tickValues))
+        .call(tickAdjust)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+            .attr("x", marginLeft)
+            .attr("y", marginTop + marginBottom - height - 6)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .attr("font-size", '12px')
+            .text(title));
+}
+
+const delay = (time) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+};
+
+function wrap(text, width) {
+    text.each(function () {
+        let text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1, // ems
+            y = text.attr("y"),
+            x = text.attr("x"),
+            dy = 1,
+            tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+            }
+        }
+    });
+}
+
+export { getISOString, isoToDate, legend }
