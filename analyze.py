d7a161308cb483907dc17146319c1a46770320b6
erxclau <ericlau41802@gmail.com>
2022-03-18 03:20:39 -0400

    Reorganize analyze.py

--- analyze.py
+++ analyze.py
@@ -1,27 +1,168 @@
-import os
+from os import scandir
+from os.path import dirname, abspath
 from time import time
 from json import load, dump
 from datetime import datetime, timedelta
 from functools import reduce
 
-import nltk
-import emoji
-import regex
+from nltk import FreqDist
+from emoji import UNICODE_EMOJI
+from regex import findall
 
-fp = os.path.dirname(os.path.abspath(__file__))
-inbox = f"{fp}/messages/inbox"
+
+def generate_count_list(tokens, limit):
+    vocab = FreqDist(tokens)
+    common = vocab.most_common(limit)
+    return [{'text': word[0], 'count': word[1]} for word in common if word[1] > 1]
+
+
+def decode_str(text: str):
+    return text.encode("latin-1").decode("utf-8")
+
+
+def is_content(message: dict):
+    msg_type: str = message.get("type", "")
+    return msg_type == "Generic" or msg_type == "Share"
 
 
 def strtodate(string: str):
     return datetime.fromisoformat(string)
 
 
+class Conversation():
+
+    def __init__(self, path: str):
+        self.path: str = path
+        self.name: str = str()
+        self.messages: list[dict] = list()
+        self.dates: dict[str, int] = dict()
+        self.minutes: list[int] = [0 for _ in range(60 * 24)]
+        self.senders: list[dict] = list()
+
+        self.emoji: dict[str, int] = dict()
+        self.emoji_count = 0
+        self.avg_text_length = 0
+
+        files = [f.name for f in scandir(path) if f.is_file()]
+
+        for file in files:
+            with open(f"{path}/{file}", encoding="latin-1") as f:
+                conversation = load(f)
+                if not self.name:
+                    self.set_name(conversation["title"])
+                msgs = conversation['messages']
+                self.messages.extend([m for m in msgs if is_content(m)])
+
+    def set_name(self, title: str):
+        self.name = decode_str(title)
+
+    def analyze(self):
+        self.parse_messages()
+        self.process_language()
+        self.find_streak()
+        self.find_max()
+
+    def parse_messages(self):
+        split: dict[str, int] = dict()
+
+        for message in self.messages:
+            t = int(message["timestamp_ms"]) / 1000
+            d = datetime.fromtimestamp(t)
+            date = d.date().isoformat()
+
+            self.dates[date] = self.dates.get(date, 0) + 1
+            self.minutes[(d.hour * 60) + d.minute] += 1
+
+            name = message["sender_name"]
+            split[name] = split.get(name, 0) + 1
+
+        self.senders = [{
+            'name': decode_str(name),
+            'number': split[name],
+            'percent': split[name] / len(self.messages) * 100
+        } for name in split.keys()]
+
+    def get_avg_text_length(self):
+        content = self.content
+        length = reduce(lambda acc, text: acc + len(text), content, 0)
+        self.avg_text_length = length / len(content) if len(content) > 0 else 0
+
+    def analyze_emoji(self):
+        text = " ".join(self.content)
+        for word in findall(r'\X', text):
+            if any(char in UNICODE_EMOJI for char in word):
+                self.emoji_count += 1
+                self.emoji[word] = self.emoji.get(word, 0) + 1
+
+    def process_language(self):
+        self.content: list[str] = [decode_str(d['content'])
+                                   for d in self.messages if 'content' in d]
+
+        self.get_avg_text_length()
+        self.analyze_emoji()
+
+        common_vocab = FreqDist(self.content)
+        self.common_count = generate_count_list(common_vocab, 100)
+        self.common_total = reduce(
+            lambda acc, t: acc + t['count'], self.common_count, 0)
+
+    def find_streak(self):
+        dates = sorted([strtodate(date) for date in self.dates.keys()])
+        one_day = timedelta(days=1)
+        streak = longest_streak = 1
+        start = end = longest_start = longest_end = dates[0]
+        for i in range(len(dates) - 1):
+            diff = dates[i+1] - dates[i]
+            if diff <= one_day:
+                streak += 1
+                end = dates[i+1]
+                if streak >= longest_streak:
+                    longest_streak = streak
+                    longest_start = start
+                    longest_end = end
+            else:
+                streak = 1
+                start = dates[i+1]
+                end = dates[i+1]
+        self.streak = {
+            'length': longest_streak,
+            'start': longest_start.date().isoformat(),
+            'end': longest_end.date().isoformat()
+        }
+
+    def find_max(self):
+        date = max(self.dates, key=self.dates.get)
+        value = self.dates[date]
+        self.max = {'date': date, 'value': value}
+
+    def get_analysis(self):
+        self.analyze()
+        return {
+            'name': self.name,
+            'dates': self.dates,
+            'minutes': self.minutes,
+            'senders': self.senders,
+            'emoji': {
+                'count': self.emoji_count,
+                'emojis': self.emoji
+            },
+            'text_count': {
+                'count': self.common_count,
+                'total': self.common_total
+            },
+            'text_length': self.avg_text_length,
+            'streak': self.streak,
+            'max': self.max,
+            'total': len(self.messages)
+        }
+
+
 def find_percentage_by_day(data, totals):
     for c_id in data.keys():
         convo = data[c_id]
         convo['percent_per_day'] = dict()
-        for date in convo['msgs_by_date'].keys():
-            percent = convo['msgs_by_date'][date] / totals[date] * 100
+        for date in convo['dates'].keys():
+            percent = convo['dates'][date] / totals[date] * 100
             convo['percent_per_day'][date] = percent
 
 
@@ -35,7 +176,7 @@ def find_per_info(data):
     convo_ids = data.keys()
     for c_id in convo_ids:
 
-        msgs = data[c_id]['msgs_by_date']
+        msgs = data[c_id]['dates']
         for date in msgs.keys():
             if not date in stack_msgs_by_date:
                 stack_msgs_by_date[date] = dict()
@@ -105,205 +246,46 @@ def find_per_info(data):
     return increment, msg_list, total_by_day
 
 
-def parse(messages):
-    dates: dict[str, int] = dict()
-    minutes = [0 for _ in range(60 * 24)]
-    split_dict: dict[str, int] = dict()
+def get_data(inbox: str):
+    conversations: dict[str, dict] = dict()
+    total: int = 0
 
-    for message in messages:
-        t = int(message['timestamp_ms']) / 1000
-        d = datetime.fromtimestamp(t)
-        date = d.date().isoformat()
+    paths: list[str] = [f.name for f in scandir(inbox)]
+    for path in paths:
+        c = Conversation(f'{inbox}/{path}')
 
-        dates[date] = dates.get(date, 0) + 1
-        minutes[(d.hour * 60) + d.minute] += 1
-
-        name = message['sender_name']
-        split_dict[name] = split_dict.get(name, 0) + 1
-
-    split = [{
-        'name': name.encode('latin-1').decode('utf-8'),
-        'number': split_dict[name],
-        'percent': split_dict[name] / len(messages) * 100
-    } for name in split_dict.keys()]
-
-    return dates, minutes, split
-
-
-def find_current_percentage(data, total):
-    for d in data:
-        d['percent'] = d['number'] / total * 100
-
-    return sorted(data, key=lambda x: x['number'], reverse=True)
-
-
-def get_emoji_analysis(content: list[str]):
-    text = " ".join(content)
-    emoji_dict: list[str, int] = dict()
-    emoji_list: list[str] = list()
-    count = 0
-    for word in regex.findall(r'\X', text):
-        if any(char in emoji.UNICODE_EMOJI for char in word):
-            count += 1
-            emoji_dict[word] = emoji_dict.get(word, 0) + 1
-            emoji_list.append(word)
-    return emoji_dict, emoji_list, count
-
-
-def generate_count_list(tokens, limit):
-    vocab = nltk.FreqDist(tokens)
-    common = vocab.most_common(limit)
-    return [{'text': word[0], 'count': word[1]} for word in common if word[1] > 1]
-
-
-def get_text_length(content):
-    length = reduce(lambda acc, text: acc + len(text), content, 0)
-    return length / len(content) if len(content) > 0 else 0
-
-
-def get_lang_processing(messages: list[dict]):
-    content: list[str] = [d['content'].encode('latin-1').decode('utf-8')
-                          for d in messages if 'content' in d]
-
-    text_length = get_text_length(content)
-
-    emoji_dict, emoji_list, emoji_count = get_emoji_analysis(content)
-
-    common_vocab = nltk.FreqDist(content)
-    common_count = generate_count_list(common_vocab, 100)
-    common_total = reduce(lambda acc, t: acc + t['count'], common_count, 0)
-
-    return {
-        'emoji': {
-            'breakdown': emoji_dict,
-            'list': emoji_list,
-            'count': emoji_count
-        },
-        'text_count': {
-            'count': common_count,
-            'total': common_total
-        },
-        'text_length': text_length
-    }
-
-
-def find_streak(dates: list[str]):
-    dates: list[datetime] = sorted([strtodate(date) for date in dates])
-    one_day = timedelta(days=1)
-    streak = longest_streak = 1
-    start = end = longest_start = longest_end = dates[0]
-    for i in range(len(dates) - 1):
-        diff = dates[i+1] - dates[i]
-        if diff <= one_day:
-            streak += 1
-            end = dates[i+1]
-            if streak >= longest_streak:
-                longest_streak = streak
-                longest_start = start
-                longest_end = end
-        else:
-            streak = 1
-            start = dates[i+1]
-    return {
-        'length': longest_streak,
-        'start': longest_start.date().isoformat(),
-        'end': longest_end.date().isoformat()
-    }
-
-
-def get_max_day(msgs_by_date: dict[str, int]):
-    date = max(msgs_by_date, key=msgs_by_date.get)
-    value = msgs_by_date[date]
-    return {'date': date, 'value': value}
-
-
-def get_conversation(directory: str):
-    name = str()
-    messages: list[dict] = list()
-
-    path = f"{inbox}/{directory}"
-    msg_files = [f.name for f in os.scandir(path) if f.is_file()]
-
-    for msg_file in msg_files:
-        with open(f"{path}/{msg_file}", encoding="latin-1") as file:
-            f = load(file)
-            if not name:
-                name = str(f['title'].encode('latin-1').decode('utf-8'))
-            messages.extend(f['messages'])
-
-    return name, messages
-
-
-def aggregate_data():
-    convo_dirs = [f.name for f in os.scandir(inbox)]
-
-    current_percent = list()
-    conversations = dict()
-    total = 0
-
-    for convo in convo_dirs:
-        name, messages = get_conversation(convo)
-        print('Analyzing messages from', name)
-
-        subtotal = len(messages)
+        subtotal = len(c.messages)
         total += subtotal
-        current_percent.append({'number': subtotal, 'name': name})
 
-        msgs_by_date, msgs_by_minute, msg_split = parse(messages)
-        streak_info = find_streak(list(msgs_by_date.keys()))
-        lang_processing = get_lang_processing(messages)
-        max_msgs_in_day = get_max_day(msgs_by_date)
+        print('Analyzing messages from', c.name)
+        conversations[path] = c.get_analysis()
 
-        conversations[convo] = {
-            'name': name,
-            'msgs_by_date': msgs_by_date,
-            'msgs_by_minute': msgs_by_minute,
-            'split': msg_split,
-            'emoji': lang_processing['emoji'],
-            'text_count': lang_processing['text_count'],
-            'text_length': lang_processing['text_length'],
-            'streak': streak_info,
-            'max': max_msgs_in_day,
-            'total': subtotal
-        }
-
-    return current_percent, conversations, total
-
-
-def create_data_dump(data):
-
-    current_percent, conversations, total = data
+    for c in conversations:
+        conversations[c]['percent'] = conversations[c]['total'] / total * 100
 
     increment, msgs_per, total_per_day = find_per_info(conversations)
     find_percentage_by_day(conversations, total_per_day)
-    current_percent = find_current_percentage(current_percent, total)
 
-    content = {
+    return {
+        'total': total,
+        'individual': conversations,
         'collective': {
-            'total': total,
-            'current_percent': current_percent,
-            'msgs_per': {
-                'data': msgs_per,
-                'increment': increment
-            },
+            'data': msgs_per,
+            'increment': increment
         },
-        'individual': {
-            'info': conversations
-        }
     }
-
-    return content
 
 
 if __name__ == "__main__":
     start = time()
 
-    writefile = f"{fp}/data.json"
+    filepath = dirname(abspath(__file__))
+    inbox = f"{filepath}/messages/inbox"
+    writefile = f"{filepath}/data.json"
 
-    data = aggregate_data()
-    content = create_data_dump(data)
+    data = get_data(inbox)
 
     with open(writefile, 'w', encoding='utf-8') as file:
-        dump(content, file, ensure_ascii=False)
+        dump(data, file, ensure_ascii=False)
 
     print(time() - start, "seconds")
